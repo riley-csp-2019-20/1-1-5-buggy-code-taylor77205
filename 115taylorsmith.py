54d5a117032a965e7a6f587cdb4af50a65cdf9c4
taylor77205 <55501027+taylor77205@users.noreply.github.com>
2019-09-24 11:25:41 -0400

    Add files via upload

--- 115taylorsmith.py
+++ 115taylorsmith.py
@@ -1,19 +1,27 @@
 #   a115_buggy_image.py
 import turtle as trtl
 
-x = trtl.Turtle()
-x.pensize(40)
-x.circle(20)
-w = 6
-y = 70
-z = 380 / w
-x.pensize(5)
-n = 0
-while (n < w):
-  x.goto(0,0)
-  x.setheading(z*n)
-  x.forward(y)
-  n = n + 1
-x.hideturtle()
+spider = trtl.Turtle()
+#create spider bofy
+spider.pensize(40)
+spider.circle(20)
+#create spider body
+spider.penup()
+spider.goto(20,30)
+spider.pendown()
+spider.circle(5)
+#create spider legs 
+legs = 8
+place = 70
+angle = 360 / legs
+spider.pensize(5)
+number = 0
+while (number < legs):
+  spider.goto(0,20)
+  spider.setheading(place*number-30/2)
+  spider.setheading(place*number+30/2)
+  spider.forward(place)
+  number = number + 1
+spider.hideturtle()
 wn = trtl.Screen()
 wn.mainloop()
